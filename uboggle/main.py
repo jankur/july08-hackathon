6472083964e5a348c78b759b238e24895e5bc18d
Diwaker Gupta <diwaker@floatingsun.net>
2008-07-05 12:40:48 -0700

    Skeleton template

--- uboggle/main.py
+++ uboggle/main.py
@@ -1,12 +1,24 @@
 #!/usr/bin/env python
 
+# Standard Python imports
 import wsgiref.handlers
+import os
 
+# GAE imports
 from google.appengine.ext import webapp
+from google.appengine.ext.webapp import template
 
 class MainHandler(webapp.RequestHandler):
   def get(self):
-    self.response.out.write('Hello world!')
+    template_values = {
+        'board': ['A', 'D', 'Qu', 'G', 'F',
+          'A', 'D', 'Qu', 'G', 'F',
+          'A', 'D', 'Qu', 'G', 'F',
+          'A', 'D', 'Qu', 'G', 'F',
+          'A', 'D', 'Qu', 'G', 'F']
+        }
+    path = os.path.join(os.path.dirname(__file__), 'index.html')
+    self.response.out.write(template.render(path, template_values))
 
 def main():
   application = webapp.WSGIApplication([('/', MainHandler)],
