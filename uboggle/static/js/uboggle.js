d3337c71e0fbd4d31ac01048ed014b64788d931d
Diwaker Gupta <diwaker@floatingsun.net>
2008-07-05 19:37:51 -0700

    Some more error handling

--- uboggle/static/js/uboggle.js
+++ uboggle/static/js/uboggle.js
@@ -1,7 +1,67 @@
-var words = ["foo", "bar", "baz"];
+var words = [];
+
+function existsOnBoard(new_word) {
+  // make sure it is a valid word on the board
+  index = lower_case_board.indexOf(new_word.charAt(0));
+  if (index == -1) {
+    $.jGrowl("This word is not on the board!",
+             {life: 2000});
+    $("#new_word").css("border", "thin solid red");
+    $("#new_word").val("");
+    return;
+  }
+  x = index / 4 | 0; // convert to int
+  y = index % 4;
+  alive = true;
+  for(i = 1; i < new_word.length && alive; i++) {
+    // there might be multiple places where this letter is
+    var foundIndices = []
+    var nIndex = lower_case_board.indexOf(new_word.charAt(i));
+    while (nIndex != -1) {
+      foundIndices.push(nIndex);
+      nIndex = lower_case_board.indexOf(new_word.charAt(i), ++nIndex);
+    }
+
+    for(j = 0; j < foundIndices.length && alive; j++) {
+      if (j == -1) {
+        alive = false;
+        break;
+      }
+      nX = j / 4 | 0; // convert to int
+      nY = j % 4;
+      if((x > 0 && nX == x-1 && nY == y) ||
+         (x < 3 && nX == x+1 && nY == y) ||
+         (y > 0 && nX == x && nY == y-1) ||
+         (y < 3 && nX == x && nY == y+1) ||
+         (x > 0 && y > 0 && nX == x-1 && nY == y-1) ||
+         (x < 3 && y < 3 && nX == x+1 && nY == y+1) ||
+         (x > 0 && y < 3 && nX == x-1 && nY == y+1) ||
+         (x < 3 && y > 0 && nX == x+1 && nY == y-1)) {
+        x = nX;
+        y = nY;
+      }
+      else {
+        $.jGrowl(nX + "," + nY + " | " + x + "," + y);
+        $.jGrowl(lower_case_board[nIndex] + "," +
+                 lower_case_board[index] + "," +
+                 new_word[i] + "," + new_word[i-1]);
+        alive = false;
+        break;
+      }
+    }
+  }
+  if(!alive) {
+    $.jGrowl("This word is not on the board!",
+             {life: 2000});
+    $("#new_word").css("border", "thin solid red");
+    $("#new_word").val("");
+    return;
+  }
+}
 
 function addWordToList() {
-  var new_word = $("#new_word").val();
+  var new_word = $("#new_word").val().toLowerCase();
+
   if (new_word.length < 3) {
     $.jGrowl("Words should be at least 3 characters!",
              {life: 2000});
@@ -19,14 +79,13 @@ function addWordToList() {
     return;
   }
 
-  // make sure it is a valid word on the board
+
   words.push(new_word);
   $("#input_words p").append(new_word + ", ");
   $("#new_word").val("");
 }
 
 function initialize() {
-  $("#submit_new_word").click(addWordToList);
   $("#countdown").countdown({until: 120, format: 'S', onExpiry: gameOver});
 }
 
